b5579d53bd41fc36e0596bb25e57e73de890df7c
Geiner Rodriguez <rodriguezmg12@hotmail.com>
2021-05-24 21:29:25 -0600

    Game ready

---
+++ script.js
@@ -0,0 +1,68 @@
+
+let randomNumber = Math.floor(Math.random() * 100) + 1
+
+const guesses = document.querySelector('.guesses')
+const lastResult = document.querySelector('.lastResult')
+const lowOrHi = document.querySelector('.lowOrHi')
+
+const guessSubmit = document.querySelector('.guessSubmit')
+const guessField = document.querySelector('.guessField')
+
+let guessCount = 1
+let resetButton
+
+function checkGuess() {
+    let userGuess = Number(guessField.value);
+    if (userGuess === randomNumber) {
+        lastResult.textContent = 'Congratulations! You got it right!'
+        lastResult.style.backgroundColor = 'green'
+        lowOrHi.textContent = ''
+        setGameOver()
+    } else if (guessCount === 10) {
+        lastResult.textContent = 'Game Over!!!'
+        setGameOver()
+    } else {
+        lastResult.textContent = 'Wrong!'
+        lastResult.style.backgroundColor = 'red'
+        if (userGuess < randomNumber) {
+            lowOrHi.textContent = 'Too low!'
+        } else {
+            lowOrHi.textContent = 'Too high!'
+        }
+    }
+
+    guessCount ++
+    guessField.value = ''
+    guessField.focus()
+}
+
+guessSubmit.addEventListener('click', checkGuess)
+
+function setGameOver() {
+    guessField.disabled = true
+    guessSubmit.disabled = true
+    resetButton = document.createElement('button')
+    resetButton.textContent = 'Start new game'
+    document.body.append(resetButton)
+    resetButton.addEventListener('click', resetGame)
+}
+
+function resetGame() {
+    guessCount = 1
+
+    const resetParas = document.querySelectorAll('.resultParas p')
+    for(let i = 0; i < resetParas.length; i ++) {
+        resetParas[i].textContent = '' 
+    }
+
+    resetButton.parentNode.removeChild(resetButton)
+
+    guessField.disabled = false
+    guessSubmit.disabled = false
+    guessField.value = ''
+    guessField.focus()
+
+    lastResult.style.backgroundColor = 'white'
+
+    randomNumber = Math.floor(Math.random() * 100) + 1
+}
